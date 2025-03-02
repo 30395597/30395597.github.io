779d5a546817e37904d41a7a5f04c2b55741c7ae
30395597 <weige111@hotmail.com>
2025-03-02 16:47:49 +0800

    Site updated: 2025-03-02 16:47:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/02/hello-world/","2025/03/02/我要发布自己的博客文章/"];function toRandomPost(){
+var posts=[];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
