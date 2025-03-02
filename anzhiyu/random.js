e3386722872f2f5b05097978edb64e53eecbec10
30395597 <weige111@hotmail.com>
2025-03-02 17:20:01 +0800

    Site updated: 2025-03-02 17:20:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/02/我家的宝贝/"];function toRandomPost(){
+var posts=["2025/03/02/《出师表》白话版，孔明吐血了！/","2025/03/02/我家的宝贝/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
