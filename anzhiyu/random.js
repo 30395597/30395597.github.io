27cb9f8c64de628393154fb11c41208416dd5ba1
30395597 <weige111@hotmail.com>
2025-03-07 22:15:31 +0800

    Site updated: 2025-03-07 22:15:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/07/我想看看过去的风景/","2025/03/02/卖米/","2025/03/02/我家的宝贝/"];function toRandomPost(){
+var posts=["2025/03/02/卖米/","2025/03/02/我家的宝贝/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
