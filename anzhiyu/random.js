6ee32cae32d9bb3e3cbe2e1f59616a633f63bb6d
30395597 <weige111@hotmail.com>
2025-03-07 22:35:11 +0800

    Site updated: 2025-03-07 22:35:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/07/牛逼顿的一生/","2025/03/02/卖米/","2025/03/02/我家的宝贝/"];function toRandomPost(){
+var posts=["2025/03/02/卖米/","2025/03/02/我家的宝贝/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
